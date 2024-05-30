735ec254a018cd1fd0e89228b697421cd7f714b1
sergoMedvedev <sergo.ranes1@mail.ru>
2024-05-31 02:40:45 +0300

    update file

---
+++ audit/Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="sergey"
+
+ENTRYPOINT ["top", "-b"]
--- audit/src/main.go
+++ audit/src/main.go
@@ -1,1 +1,8 @@
 package src
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.Run(":8080")
+}
